b195de5153ac3630c5c408b2686f004b992042bf
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-03-02 01:22:12 +0300

    Добавьте файлы проекта.

---
+++ lesson6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задача1 Найти самое длинное слово в строке
+
+            string str = Console.ReadLine();
+            string[] strArray = str.Split();
+            string max = "";
+
+            foreach(string s in strArray)
+            {
+                if (s.Length > max.Length)
+                {
+                    max = s;
+                }
+            }
+            
+            Console.WriteLine("Слово максимальной длины  {0}", max);
+            #endregion
+
+            Console.Write("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задача2 Составить программу, определяющую является ли строка палиндромом без учёта пробелов и регистра
+            string str = Console.ReadLine();
+            string processedStr = str.Replace(" ", "").ToLower();
+            int i = 0, j = processedStr.Length-1;
+            bool isPalindrom = true;
+            while ((isPalindrom) &&(i < j))
+            {
+                if (processedStr[i]!= processedStr[j])
+                {
+                    isPalindrom = false;
+                }
+                i++; j--;
+            }
+
+            Console.WriteLine("Является ли строка палиндромом? -  {0}", isPalindrom);
+            #endregion
+
+            Console.Write("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
